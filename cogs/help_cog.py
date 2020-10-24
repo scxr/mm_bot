4c3854d9f7af518baffb855ee25a73588c377311
xo <56652842+scxr@users.noreply.github.com>
2020-10-24 14:25:23 +0100

    Update help_cog.py

--- cogs/help_cog.py
+++ cogs/help_cog.py
@@ -9,8 +9,8 @@ class HelpCog(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         help_embed = discord.Embed(title='Help!', colour=randint(0,0xFFFFFF))
-        help_embed.add_field(name='Start A Deal', value='`!deal @member value` Please note, value must be an integer with no dollar sign or anything after')
-        help_embed.add_field(name='View current transaction fees', value='`!fees` This shows current recommended transaction fees fetched from : `https://bitcoinfees.earn.com/api/v1/fees/recommended`')
+        help_embed.add_field(name='Start A Deal', value='`!deal @member value` Please note, value must be an integer with no dollar sign or anything after', inline=False)
+        help_embed.add_field(name='View current transaction fees', value='`!fees` This shows current recommended transaction fees fetched from : `https://bitcoinfees.earn.com/api/v1/fees/recommended`', inline=False)
         help_embed.set_footer(text='Made by xo#0111')
         await ctx.send(embed=help_embed)
 
